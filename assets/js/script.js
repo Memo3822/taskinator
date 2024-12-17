e4c540907d341c2893f3ab86c188ec1019a053db
Memo <epera023@gmail.com>
2024-12-17 18:38:29 -0500

    finalized drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,6 +208,7 @@ var dropZoneDragHandler = function(event) {
 
    if(taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
         console.dir(taskListEl);
     }
 };
@@ -231,7 +232,15 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      taskListEl.removeAttribute("style");
+    }
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -241,3 +250,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
